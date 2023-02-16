7ae96c792a691b5d5c1819e187e38fc0163ec222
Arpit Dawar <arpitdawar@XR434NJ0P3s-MacBook-Pro.local>
2023-02-16 13:03:00 +0530

    Project Setup & added add API

--- watiprojAPI/API/apiviews.py
+++ watiprojAPI/API/apiviews.py
@@ -3,7 +3,7 @@ from rest_framework import response
 from rest_framework.permissions import AllowAny
 
 from API.serializers import SumSerializer
-
+from API.models import sumLogs
 
 class SumIntegers(APIView):
   permission_classes = [AllowAny]
@@ -16,4 +16,8 @@ class SumIntegers(APIView):
     print(serialized.data)
     num1=serialized.data.get('num1')
     num2=serialized.data.get('num2')
-    return response.Response({'sum':num1+num2})
+    sum = num1+num2
+    log = sumLogs.objects.create(number1=num1, number2=num2, sum=sum)
+    print(log)
+    # print(sumLogs.objects.all())
+    return response.Response({'sum':num1+num2, 'storageKey':log.pk})
--- watiprojAPI/watiprojAPI/settings/base.py
+++ watiprojAPI/watiprojAPI/settings/base.py
@@ -45,10 +45,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'API',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
+        "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -79,7 +82,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "watiprojAPI.wsgi.application"
-
+CORS_ALLOW_ALL_ORIGINS=True
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
